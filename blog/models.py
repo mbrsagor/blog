7af7d3a58157a5c0762c436fc9e419b0e3a67cfb
Faysal Hossain <fsfoysal15@gmail.com>
2020-08-28 18:15:50 +0600

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
 class Post(models.Model):
-    author = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    #author = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=70)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,blank=True,null=True)
+    image = models.ImageField(upload_to='post/images',blank=True,null=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
