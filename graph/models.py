5d6426f8f80c08e032cbdfce3319e70716381600
Sagor <mbrsagor@gmail.com>
2020-10-30 23:50:54 +0600

    Product QL

--- graph/models.py
+++ graph/models.py
@@ -3,3 +3,15 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, null=True,  on_delete=models.CASCADE, related_name='product_category')
+
+    def __str__(self):
+        return self.name
--- graph/schema.py
+++ graph/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryType(DjangoObjectType):
@@ -10,8 +10,18 @@ class CategoryType(DjangoObjectType):
         fields = ("id", "name")
 
 
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'category')
+
+
 class Query(graphene.ObjectType):
     categories = graphene.List(CategoryType)
+    products = graphene.List(ProductType)
 
     def resolve_categories(self, info, **kwargs):
         return Category.objects.all()
+
+    def resolve_products(self, info, **kwargs):
+        return Product.objects.all()
